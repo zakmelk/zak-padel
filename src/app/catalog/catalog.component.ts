4778875967a94fca7f9a9708f9d4fc6fca7ba17b
zakmelk <melkbox@gmail.com>
2025-12-17 18:38:10 +0100

    zmk001 : add catalog and header

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pad-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+
+}
---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pad-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css']
+})
+export class SiteHeaderComponent {
+
+  constructor() { }
+
+}
