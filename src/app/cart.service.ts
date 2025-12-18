eb3e9d00e56828d86b34aca9a5039dfd28ba7de8
zakmelk <melkbox@gmail.com>
2025-12-18 13:11:08 +0100

    zmk001 : Get and Post http requests

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  cart: IProduct[] = [];
+  private cart: IProduct[] = [];
+  
+  constructor(private http: HttpClient) { }
+
 
   addToCart(product: IProduct){
       this.cart.push(product);
-      console.log(`product ${product.name} added to cart`);
+      this.http.post('/api/cart', this.cart).subscribe(()=> {
+        console.log(`product ${product.name} added to cart`);
+      });
   }
-  
-  constructor() { }
+
 }
