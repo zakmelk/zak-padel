38de84b6dbc69ee3f2e9bfdf4a72d85f227dce57
zakmelk <melkbox@gmail.com>
2025-12-19 11:02:33 +0100

    zmk001 : routing parameters

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'pad-catalog',
@@ -16,13 +16,17 @@ export class CatalogComponent {
   constructor(
     private cartSvc: CartService, 
     private productSvc: ProductService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
   }
 
   ngOnInit(){
     this.productSvc.getProducts().subscribe(products => {
       this.products = products;
+    });
+    this.route.params.subscribe((params) =>{
+      this.filter = params['filter'] ?? '';
     });
   }
 
