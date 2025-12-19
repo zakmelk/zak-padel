3357b7bc5bace2edbaadcd3f734422180fe1223b
zakmelk <melkbox@gmail.com>
2025-12-19 10:36:17 +0100

    zmk001 : add routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { CartComponent } from './cart/cart.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Zak's Padel"},
   { path: 'catalog', component: CatalogComponent, title: "Catalog - Zak's Padel"},
-  { path: 'cart', component: CartComponent, title: "Cart - Zak's Padel"}
+  { path: 'cart', component: CartComponent, title: "Cart - Zak's Padel"},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
